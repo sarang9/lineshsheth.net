2c7f9fb745d6b789d42b8405c5cca4e90a11584a
sarang9 <narangiur@gmail.com>
2020-12-08 21:33:02 +0530

    2 commit

---
+++ why - Copy.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include"header.php"; ?>
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Metrophobic" rel="stylesheet"> 
+
+  </head>
+  <body class="animsition">
+    
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron header">
+        <h2 align="center" style="color:#fff;">A Life Of</h2>
+        <h1 align="center" style="color:#2DBA9F;">Learning, Relating and Gratitude</h1>
+        <h3 align="center" style="color:#fff;">With Linesh Sheth</h3>
+      </div>
+
+      <div id="menuzord" class="menuzord red" align="center">
+        <!-- <a href="javascript:void(0)" class="menuzord-brand">Menuzord</a> -->
+        <ul class="menuzord-menu" align="center">
+          
+          <li class="active"><a href="index">ABOUT</a></li>
+          <li class=""><a href="soil">SOIL</a></li>
+          <li class=""><a href="masters/masters">MASTERS</a></li>
+          <li class=""><a href="family/fam">FAMILY</a></li>
+          <li class=""><a href="colleagues/colleagues">COLLEAGUES</a></li>
+          <li class=""><a href="testimonies">TESTIMONIES</a></li>
+          <li class=""><a href="support">CONTACT</a></li>
+        </ul>
+      </div>
+
+
+      
+      <div class="divider">&nbsp;</div>
+      
+      <div class="container">
+        <div class="content">
+          <div class="row">
+            <div class="col-lg-12">
+              <h1 align="center" style="color:#2277BC; font-family: 'Metrophobic', sans-serif;">Why this website? </h1>
+                <div class="col-lg-12" style="font-family: 'Metrophobic', sans-serif; font-size:17px; text-align:justify;">
+                  
+                  <div class="col-lg-3" style="margin:0px 10px; padding:0px;">
+                    <img class="img-responsive" src="images/20170104_151409.jpg">
+                  </div>
+                  <div class="col-lg-3 pull-right" style="margin:0px 10px; padding:0px;">
+                    <img class="img-responsive" src="images/history.jpg">
+                  </div>
+                  
+<p>Years back when i was in high school, my history teacher asked me to name the Mughal Empire successors in india. As a part of my study of Mughal history, i was able to name seven successors from Babar down to Bahadur Shah Zafar II. Then my teacher asked me to name seven successors of my family. I was dumb-founded as a child!. I did not know anything beyond my grandfather, who was just my second generation. When the teacher further asked me what i knew about the lives of my father and grandfather, i was further surprised that i hardly knew about their lives.</p>
+
+<p>Today i understand that we are living with scant regards for our family history. We hardly know about the particularities, the characters and the conditions in which our immediate ancestors lived. I realize that we owe large part of who we are to the generations that preceded us as our fathers, grandfathers, great grandfathers and so on. Yet we hardly know about them.</p>
+
+<div class="col-lg-2 pull-right" style="margin:0px 10px; padding:0px;">
+  <img class="img-responsive" src="images/contri.jpg">
+</div>
+<p>Therefore this website is made to record who all are the people that i call as my family, what are their contributions to my life and what are their characteristics. I realize that i am because of contributions made by members of my family, many masters from whom i learnt, several colleagues i worked with and friends i lived with. I have tried to interview many of the people i have listed here, studied their lives and have tried to record them as authentically as i can. Idea is that generations to come should be able to know people i lived with and who contributed to who i am.</p>
+
+<p>This website, although looks personal, is an attempt at showing the significance of knowing who we are in context of the people who have contributed to our lives. This is one example. I wish, those who go through this, are inspired by the importance of knowing our history and would try to leave their own stories as a examples to follow by making their own website.</p>
+
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="divider">&nbsp;</div>
+
+
+
+        
+
+      </div>
+    
+    <?php include"footer.php"; ?>
+
+  </body>
+</html>
+
